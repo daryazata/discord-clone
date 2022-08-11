2d9273e88db232a966ad71f65eb142ab82e815f6
Darya Zata <darya.zata@outlook.com>
2022-08-11 17:12:23 +0200

    Updates

---
+++ precache-manifest.dca6162e06b7e57ccbfa535910ca7922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ded35b602668a2a9c81eaa62cbb712",
+    "url": "/discord-clone/index.html"
+  },
+  {
+    "revision": "c986760264c8fbbe59b6",
+    "url": "/discord-clone/static/css/main.db750740.chunk.css"
+  },
+  {
+    "revision": "6170a17a21bb7b0e8c67",
+    "url": "/discord-clone/static/js/2.b87b8197.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/discord-clone/static/js/2.b87b8197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c986760264c8fbbe59b6",
+    "url": "/discord-clone/static/js/main.e4010637.chunk.js"
+  },
+  {
+    "revision": "7513c738c2e2ffe4d32a",
+    "url": "/discord-clone/static/js/runtime-main.f533448a.js"
+  }
+]);
